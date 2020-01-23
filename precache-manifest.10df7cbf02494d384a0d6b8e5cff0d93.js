ed84f20741d4f437c47bbdc7d7c4632124426f47
i-sundutova <60006899+i-sundutova@users.noreply.github.com>
2020-01-23 09:59:03 +0200

    Updates

---
+++ precache-manifest.10df7cbf02494d384a0d6b8e5cff0d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7baaa662fec186efa5aebfbc6554e819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dbe2fca7deb7d78b0d1",
+    "url": "/static/css/main.bd9ee6d3.chunk.css"
+  },
+  {
+    "revision": "af505f3a0f22c511fd46",
+    "url": "/static/js/2.4fc91aa0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4fc91aa0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dbe2fca7deb7d78b0d1",
+    "url": "/static/js/main.8a5fae28.chunk.js"
+  },
+  {
+    "revision": "c01ea40ba74afe5a8b5d",
+    "url": "/static/js/runtime-main.0c3274b9.js"
+  }
+]);
